7e8125ad471b0ceb26a824444d29c645e08674b2
Stephen Johnson-Tseu <71110517+stephjohnsons@users.noreply.github.com>
2024-11-28 11:31:42 +0800

    added price format

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ApplicationRecord
+  before_save :format_price
+
+  private
+
+  def format_price
+    if self.price.present?
+      self.price = BigDecimal(self.price.to_s).round(2).to_s
+    end
+  end
 end
